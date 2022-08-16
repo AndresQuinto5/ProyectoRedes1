654fffa47a922c744f1cd52b0e661611edff9602
Andres <qui18288@uvg.edu.gt>
2022-08-16 13:04:25 -0600

    main

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ Proyecto 1 chat con protocolo XMPP
 AndresEmilioQuinto - 18288
 
 """
+
+import os
+import logging
+import time
+import sleekxmpp
+from getpass import getpass
+from prettytable import PrettyTable
+from sleekxmpp.exceptions import IqError, IqTimeout
+
+import client
+from consts import *
+
+close_login = False
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(levelname)-8s %(message)s')
+
+
+# With tkinter, opens a windows for the user to select a file.
+def get_file_path():
+    file_path = filedialog.askopenfilename()
+    return file_path
