8adc204837e089ebf55f55518d31c4798f748dd2
Andres <qui18288@uvg.edu.gt>
2022-08-15 21:03:24 -0600

    const

--- consts.py
+++ consts.py
@@ -7,3 +7,14 @@ AndresEmilioQuinto - 18288
 """
 
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m' + 'OK: '
+OKGREEN = '\033[92m' + 'OK: '
+WARNING = '\033[93m' + 'WARNING: '
+FAIL = '\033[91m' + 'FAIL: '
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[1;30m'
+BLUE = '\033[94m'
+RED = '\033[91m'
+YELLOW = '\033[93m'
